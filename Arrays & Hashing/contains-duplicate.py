7e12e84aae4fcec0e7660982586d06f471e604fb
Murad Zalov <mzalov@revealdata.com>
2024-06-06 09:36:27 -0500

    some changes

---
+++ Arrays & Hashing/contains-duplicate.py
@@ -0,0 +1,13 @@
+# https://neetcode.io/problems/duplicate-integer
+# https://leetcode.com/problems/contains-duplicate/description/
+# Duplicate Integer
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1, 2, 3, 3]
+# Output: true
+
+# Example 2:
+# Input: nums = [1, 2, 3, 4]
+# Output: false
+
